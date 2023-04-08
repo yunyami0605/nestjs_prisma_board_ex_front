1d84ce7c455525c88a415af94c78565511a75450
yunyami <yunyami0605@gmail.com>
2023-04-08 11:05:51 +0900

    [Feat] 프로젝트 cra 셋팅
    1. 프로젝트 cra 셋팅

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {}
+function Login() {
+  return (
+    <Box>
+      <Text></Text>
+    </Box>
+  );
+}
+
+export default Login;
