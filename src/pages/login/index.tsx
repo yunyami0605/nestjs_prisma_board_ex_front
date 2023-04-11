5e7ee710704d8fca96918da0ade60820bc2cccfe
yunyami <yunyami0605@gmail.com>
2023-04-11 16:09:52 +0900

    [Feat] 로그인 퍼블 추가
    1. 로그인 퍼블 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,51 @@
-import { Box, Text } from "@chakra-ui/react";
+import FormInput from "@/components/common/form/FormInput";
+import FormLabel from "@/components/common/form/FormLabel";
+import {
+  Center,
+  FormControl,
+  Input,
+  VStack,
+  Text,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
 interface Props {}
 function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
-    <Box>
-      <Text></Text>
-    </Box>
+    <Center>
+      <VStack alignContent={"flex-start"}>
+        <FormLabel mb="10px">이메일</FormLabel>
+
+        <FormInput
+          mb="16px"
+          placeholder="이메일"
+          value={form.email}
+          onChange={(e) =>
+            setForm((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+
+        <FormLabel mb="10px">비밀번호</FormLabel>
+        <FormInput
+          mb="12px"
+          placeholder="비밀번호"
+          type="password"
+          value={form.password}
+          onChange={(e) =>
+            setForm((prev) => ({ ...prev, password: e.target.value }))
+          }
+        />
+
+        <Flex></Flex>
+      </VStack>
+    </Center>
   );
 }
 
