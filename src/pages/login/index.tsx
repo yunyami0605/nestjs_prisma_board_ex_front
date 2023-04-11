2d0c1da7795fc0b09a231ef43240068ddd8a045c
yunyami <yunyami0605@gmail.com>
2023-04-11 21:34:53 +0900

    [Feat] 로그인 페이지 퍼블 수정 및 로그인 로직 추가
    1. 로그인 페이지
    - 퍼블 수정
    - 로그인 로직 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,50 +1,80 @@
+import { usePostLogin } from "@/api/auth/mutation";
+import DefaultButtonBar from "@/components/common/button/DefaultButtonBar";
+import PositiveButtonBar from "@/components/common/button/PositiveButtonBar";
 import FormInput from "@/components/common/form/FormInput";
 import FormLabel from "@/components/common/form/FormLabel";
-import {
-  Center,
-  FormControl,
-  Input,
-  VStack,
-  Text,
-  Button,
-  Flex,
-} from "@chakra-ui/react";
+import { setToken } from "@/utils/auth";
+import { Center, Box, Stack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 
-interface Props {}
 function Login() {
-  const [form, setForm] = useState({
+  const postLogin = usePostLogin();
+  const router = useRouter();
+
+  const initForm = {
     email: "",
     password: "",
-  });
+  };
+
+  const [form, setForm] = useState(initForm);
+
+  const onLogin = () => {
+    postLogin
+      .mutateAsync(form)
+      .then((response) => {
+        if (response.statusCode === 201) {
+          setToken("ACCESS", response.data.access);
+          setToken("REFRESH", response.data.refresh);
+
+          router.replace("/posts");
+        }
+      })
+      .catch((error) => {
+        /**
+         *@todo api 401 로직 추가하기
+         */
+        console.log(error);
+      });
+  };
+
+  const onMoveSignupPage = () => router.push("/signup");
+
+  const onChangeForm = (formKey: keyof typeof initForm, value: string) => {
+    setForm((prev) => ({ ...prev, [formKey]: value }));
+  };
 
   return (
-    <Center>
-      <VStack alignContent={"flex-start"}>
-        <FormLabel mb="10px">이메일</FormLabel>
+    <Center h="70vh">
+      <Box w="310px">
+        <FormLabel w="100%" mb="10px">
+          이메일
+        </FormLabel>
 
         <FormInput
           mb="16px"
           placeholder="이메일"
           value={form.email}
-          onChange={(e) =>
-            setForm((prev) => ({ ...prev, email: e.target.value }))
-          }
+          onChange={(e) => onChangeForm("email", e.target.value)}
         />
 
-        <FormLabel mb="10px">비밀번호</FormLabel>
+        <FormLabel w="100%" mb="10px">
+          비밀번호
+        </FormLabel>
+
         <FormInput
-          mb="12px"
+          mb="24px"
           placeholder="비밀번호"
           type="password"
           value={form.password}
-          onChange={(e) =>
-            setForm((prev) => ({ ...prev, password: e.target.value }))
-          }
+          onChange={(e) => onChangeForm("password", e.target.value)}
         />
 
-        <Flex></Flex>
-      </VStack>
+        <Stack spacing={"8px"}>
+          <PositiveButtonBar name="로그인" onClick={onLogin} />
+          <DefaultButtonBar name="회원가입" onClick={onMoveSignupPage} />
+        </Stack>
+      </Box>
     </Center>
   );
 }
