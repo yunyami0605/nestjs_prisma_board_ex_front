0c2e34f8c1adc7db84f98d95f09ef53024a87a77
yunyami <yunyami0605@gmail.com>
2023-04-14 21:33:41 +0900

    [Feat] 댓글 수정, 삭제 api query hook 추가
    1. 댓글 수정, 삭제 api query hook 추가

--- src/api/comment/mutation.ts
+++ src/api/comment/mutation.ts
@@ -1,9 +1,13 @@
 import { useMutation } from "react-query";
 import { apiCall } from "../common";
-import { PostCommentData } from "@/types/api/comment";
+import {
+  PostCommentData,
+  MutationCommentResponse,
+  PatchCommentData,
+} from "@/types/api/comment";
 
 const postComment = (data: PostCommentData) => {
-  return apiCall<any>({
+  return apiCall<MutationCommentResponse>({
     method: "POST",
     url: `comment/${data.postId}`,
     data: {
@@ -12,6 +16,40 @@ const postComment = (data: PostCommentData) => {
   });
 };
 
+/**
+ *@description 댓글 등록
+ */
 export const usePostComment = () => {
   return useMutation((data: PostCommentData) => postComment(data));
+};
+
+const patchComment = (data: PatchCommentData) => {
+  return apiCall<MutationCommentResponse>({
+    method: "PATCH",
+    url: `comment/${data.commentId}`,
+    data: {
+      content: data.content,
+    },
+  });
+};
+
+/**
+ *@description 댓글 수정
+ */
+export const usePatchComment = () => {
+  return useMutation((data: PatchCommentData) => patchComment(data));
+};
+
+const deleteComment = (postId: number) => {
+  return apiCall<MutationCommentResponse>({
+    method: "DELETE",
+    url: `comment/${postId}`,
+  });
+};
+
+/**
+ *@description 댓글 삭제
+ */
+export const useDeleteComment = () => {
+  return useMutation((commentId: number) => deleteComment(commentId));
 };
