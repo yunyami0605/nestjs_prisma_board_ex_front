2ce52a9fe2e43ea1634445a51bb377af3cf83028
yunyami <yunyami0605@gmail.com>
2023-04-18 11:18:30 +0900

    [Feat] 답글 수정 삭제 api 호출 로직 추가
    1. 답글 api
    - 수정, 삭제 api 추가

--- src/api/recomment/mutation.ts
+++ src/api/recomment/mutation.ts
@@ -21,3 +21,34 @@ const postRecomment = (data: PostRecommentData) => {
 export const usePostRecomment = () => {
   return useMutation((data: PostRecommentData) => postRecomment(data));
 };
+
+const patchRecomment = (data: PostRecommentData) => {
+  return apiCall<MutationRecommentResponse>({
+    method: "PATCH",
+    url: `recomment/${data.commentId}`,
+    data: {
+      content: data.content,
+    },
+  });
+};
+
+/**
+ *@description 답글 수정
+ */
+export const usePatchRecomment = () => {
+  return useMutation((data: PostRecommentData) => patchRecomment(data));
+};
+
+const deleteRecomment = (postId: number) => {
+  return apiCall<MutationRecommentResponse>({
+    method: "DELETE",
+    url: `recomment/${postId}`,
+  });
+};
+
+/**
+ *@description 답글 삭제
+ */
+export const useDeleteRecomment = () => {
+  return useMutation((commentId: number) => deleteRecomment(commentId));
+};
